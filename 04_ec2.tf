d748ec359c2e6fd0899a4911f7745f9eb866be31
scresh <emanuel.zarzecki@gmail.com>
2024-07-24 21:28:29 +0200

    Move user_data to separate file

--- 04_ec2.tf
+++ 04_ec2.tf
@@ -28,29 +28,15 @@ resource "aws_launch_template" "launch_template" {
   vpc_security_group_ids = [aws_security_group.ec2_security_group.id]
   iam_instance_profile { arn = aws_iam_instance_profile.ec2_profile.arn }
 
-  user_data = base64encode(
-    join(
-      "\n",
-      [
-        "#!/bin/bash",
-        "set -euxo pipefail",
-        "sudo yum install -y unzip",
-        "curl 'https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip' -o 'awscliv2.zip'",
-        "unzip awscliv2.zip",
-        "sudo ./aws/install",
-        "until curl --fail http://169.254.169.254/latest/meta-data/iam/security-credentials/${aws_iam_role.ec2_role.name}; do sleep 1; done",
-        "aws ec2 wait volume-available --volume-ids ${aws_ebs_volume.storage.id} --region ${var.region}",
-        "aws ec2 attach-volume --volume-id ${aws_ebs_volume.storage.id} --instance-id $(cat /var/lib/cloud/data/instance-id) --device /dev/sdh --region ${var.region}",
-        "aws ec2 wait volume-in-use --volume-ids ${aws_ebs_volume.storage.id} --filters 'Name=attachment.status,Values=attached' --region ${var.region}",
-        "until [ -e /dev/sdh ]; do sleep 1; done",
-        "sudo blkid $(readlink -f /dev/sdh) || sudo mkfs -t ext4 /dev/sdh",
-        "sudo mkdir -p /data",
-        "echo '/dev/sdh /data ext4 defaults 0 2' | sudo tee -a /etc/fstab",
-        "sudo mount -a",
-        "echo ECS_CLUSTER='${aws_ecs_cluster.cluster.name}' > /etc/ecs/ecs.config",
-      ]
-    )
-  )
+  user_data = base64encode(templatefile("${path.module}/user_data.tftpl", {
+    ec2_role_name  = aws_iam_role.ec2_role.name
+    ebs_storage_id = aws_ebs_volume.storage.id
+    region         = var.region
+    cluster_name   = aws_ecs_cluster.cluster.name
+    device_path    = "/dev/sdh"
+    mount_path     = "/data"
+  }))
+
 }
 
 resource "aws_autoscaling_group" "autoscaling_group" {
